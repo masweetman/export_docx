f6689d19fd4762d5494acf9b42b2050f9129f8dc
Mike Sweetman <mi894527@bmrn.com>
2016-08-16 16:22:46 -0700

    bump 1.0.0

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :export_docx do
   name 'Export DOCX'
   author 'Mike Sweetman'
   description 'This plugin exports issues to DOCX files'
-  version '0.0.1'
+  version '1.0.0'
   url 'http://github.com/masweetman/export_docx'
   author_url 'http://github.com/masweetman'
   
