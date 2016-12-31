9bd4f9197330e67181b1c8b48482aefaba55cb1d
masweetman <slosweetman@gmail.com>
2016-12-31 12:08:08 -0700

    option to use one template for all trackers

--- app/controllers/docx_controller.rb
+++ app/controllers/docx_controller.rb
@@ -4,17 +4,19 @@ class DocxController < ApplicationController
   #unloadable
 
   def template_upload
+    use_for_all = params[:use_for_all]
     uploaded_io = params[:template]
     filename = params[:tracker] + '.docx'
-    if File.extname(uploaded_io.original_filename) == '.docx'
-      folder_structure
-      File.open(Rails.root.join('files', 'export_docx', 'templates', filename), 'wb') do |file|
-        file.write(uploaded_io.read)
-        flash[:notice] = filename + ' uploaded successfully.'
+    upload_file(filename, uploaded_io)
+
+    if use_for_all == '1'
+      source = Rails.root.join('files', 'export_docx', 'templates', filename)
+      Tracker.all.each do |tracker|
+        dest = Rails.root.join('files', 'export_docx', 'templates', tracker.name + '.docx')
+        FileUtils.copy_file(source, dest) unless source == dest
       end
-    else
-      flash[:error] = 'Template must be a .docx file.'
     end
+
     redirect_to plugin_settings_path(Redmine::Plugin.find('export_docx'))
   end
   
@@ -37,5 +39,19 @@ class DocxController < ApplicationController
       send_file(path_to_file)
     end
   end
+
+  private
+
+    def upload_file(filename, uploaded_io)
+      if File.extname(uploaded_io.original_filename) == '.docx'
+        folder_structure
+        File.open(Rails.root.join('files', 'export_docx', 'templates', filename), 'wb') do |file|
+          file.write(uploaded_io.read)
+          flash[:notice] = filename + ' uploaded successfully.'
+        end
+      else
+        flash[:error] = 'Template must be a .docx file.'
+      end
+    end
   
 end
